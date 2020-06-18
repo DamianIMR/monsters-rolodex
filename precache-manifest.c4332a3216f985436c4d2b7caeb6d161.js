e8948ab5a80d11ce0f3191e59f802b57115c3cbe
Damian Mercado <mercadodamian3@gmail.com>
2020-06-18 10:41:30 -0500

    Updates

---
+++ precache-manifest.c4332a3216f985436c4d2b7caeb6d161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e178abf7b6ec17a73ff4891a1fc20f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dc86edf1826f3959620b",
+    "url": "/monsters-rolodex/static/css/main.10a72124.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc86edf1826f3959620b",
+    "url": "/monsters-rolodex/static/js/main.15c03d8d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
